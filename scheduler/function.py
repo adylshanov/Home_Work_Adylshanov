6b7429b4b847904f1aab74f762dab2268ad9bcfe
Adylshanov Damir <adelshanov@gmail.com>
2018-04-26 23:31:35 +0300

    Домашка_№4.1 Ежедневник

---
+++ scheduler/function.py
@@ -0,0 +1,28 @@
+__all__ = [
+	'print_scheduler',
+	'add_scheduler',
+	'edit_scheduler',
+	'end_scheduler',
+	'begin_again_scheduler',
+]
+
+def print_scheduler():
+	"""Вывод списка задач"""
+	print('ones')
+	input()
+
+def add_scheduler():
+	"""Вывод списка задач"""
+	pass
+
+def edit_scheduler():
+	"""Вывод списка задач"""
+	pass
+
+def end_scheduler():
+	"""Вывод списка задач"""
+	pass
+
+def begin_again_scheduler():
+	"""Вывод списка задач"""
+	pass
---
+++ scheduler/main.py
@@ -0,0 +1,28 @@
+from function import *
+
+def input_menu():
+	while True: #not (number.is_integer() == False) and not (1 <= number <= 6):
+		number = input()
+		if not number.isnumeric():
+			print('Не верный ввод данных. Введите цифру от 1 до 6: ')
+		elif not (1 <= int(number) <=6):
+			print('Не верный ввод данных. Введите цифру от 1 до 6: ')
+		else:
+			return(int(number))
+
+number = 0
+clear = '\n' * 100
+while (number != 6):
+	#print(clear)
+	print('Ежедневник. Выберите действие: \n\n 1. Вывести список задач \n 2. Добавить задачу \n 3. Отредактировать задачу \n 4. Завершить задачу \n 5. Начать задачу с начала \n 6. Завершить задачу \n')
+	number = input_menu()
+	if (number == 1) : 
+		print_scheduler()
+	elif (number == 2) : add_scheduler()
+	elif (number == 3) : edit_scheduler()
+	elif (number == 4) : end_scheduler()
+	elif (number == 5) : begin_again_scheduler()
+
+ 	
+
+
