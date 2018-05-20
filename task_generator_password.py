600563859a2216dc3ca331a94ded334e4739c40a
Damir Adylshanov <adylshanov@gmail.com>
2018-05-20 23:14:28 +0300

    +Генератор паролей

--- task_generator_password.py
+++ task_generator_password.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-import random
+from random import randrange
 
-SLOVAR = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890'
+SLOVAR = list('QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890')
 
 def password_generator(number):
-    passk = ''
-    for i in range(number):
-        passk = SLOVAR[random.randrange(len(SLOVAR))-1]
-        yield passk
+    l = len(SLOVAR)
+    while 1:
+        rez = []
+        #print(SLOVAR[random.randrange(len(SLOVAR))])
+        for i in range(number):
+            rez.append(SLOVAR[randrange(l)])
+        yield ''.join(rez)
+
+if __name__ == '__main__':
+    gen = password_generator(10)
+    print(next(gen))
